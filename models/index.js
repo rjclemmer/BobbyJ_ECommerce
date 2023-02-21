316cbfc32e90cb67512def9cbc81d7d6668880c4
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-02-20 21:53:52 -0500

    more models

--- models/index.js
+++ models/index.js
@@ -5,11 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  
+})
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
