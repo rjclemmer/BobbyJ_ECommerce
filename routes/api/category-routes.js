fb3186ff79a91a6ef1232e126879cf4c53b0d73d
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-03-05 16:01:40 -0500

    rest of category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,16 +46,48 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (updateCategory) {
+      res.json(updateCategory);
+    } else {
+      res
+        .status(404)
+        .json({ error: "I'm sorry, Dave. I'm afraid I can't do that." });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
