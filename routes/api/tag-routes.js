0d6b9dac8fccc30aa3c1c6d249a175b5b49f43cd
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-03-05 16:13:14 -0500

    get tag and get tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,48 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
+    res.json(tags);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const singleTag = await Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
+    if (singleTag) {
+      res.json(singleTag);
+    } else {
+      res
+        .status(404)
+        .json({ error: "I'm sorry, Dave. I'm afraid I can't do that." });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post('/', (req, res) => {
